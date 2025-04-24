b0cde8a55205b69aae244171e704c16dd0d6efa8
Wyzebb <BenSJSmith8@gmail.com>
2025-04-24 21:37:03 +0100

    Add debug messages and move constants

--- src/main/java/me/wyzebb/asteroidSpooferUpgrades/events/FakePlayerDeath.java
+++ src/main/java/me/wyzebb/asteroidSpooferUpgrades/events/FakePlayerDeath.java
@@ -16,20 +16,25 @@ import static me.wyzebb.asteroidSpooferUpgrades.AsteroidSpooferUpgrades.plugin;
 public class FakePlayerDeath implements Listener {
 
     private final MiniMessage mm;
+    private final AccountLinkManager manager;
+    private final Guild mainGuild;
 
     public FakePlayerDeath() {
         this.mm = MiniMessage.miniMessage();
+        this.manager = DiscordSRV.getPlugin().getAccountLinkManager();
+        this.mainGuild = DiscordSRV.getPlugin().getMainGuild();
     }
 
     @EventHandler
     public void onFakePlayerDeath(FakePlayerDeathEvent event) {
-        AccountLinkManager manager = DiscordSRV.getPlugin().getAccountLinkManager();
+        plugin.getLogger().warning("Fake player death");
         String discordId = manager.getDiscordId(event.getFakePlayer().getEntityPlayer().getUniqueId());
+        plugin.getLogger().warning("Got Discord ID");
 
         Member guildMember = null;
         if (discordId != null) {
-            Guild mainGuild = DiscordSRV.getPlugin().getMainGuild();
             guildMember = mainGuild.getMemberById(discordId);
+            plugin.getLogger().warning("Found guild member");
         }
 
         String dmDeathMessage = plugin.getConfig().getString("death-messages.dm", "You just died on the server!");
@@ -38,9 +43,11 @@ public class FakePlayerDeath implements Listener {
         broadcastDeathMessage = broadcastDeathMessage.replace("{player}", event.getFakePlayer().getEntityPlayer().getName());
 
         Bukkit.broadcast(mm.deserialize(broadcastDeathMessage));
+        plugin.getLogger().warning("Broadcast sent");
 
         if (guildMember != null) {
             DiscordUtil.privateMessage(guildMember.getUser(), dmDeathMessage);
+            plugin.getLogger().warning("Private message sent");
         }
     }
 }
--- src/main/java/me/wyzebb/asteroidSpooferUpgrades/events/FakePlayerJoin.java
+++ src/main/java/me/wyzebb/asteroidSpooferUpgrades/events/FakePlayerJoin.java
@@ -5,11 +5,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import static me.wyzebb.asteroidSpooferUpgrades.AsteroidSpooferUpgrades.plugin;
+
 public class FakePlayerJoin implements Listener {
 
     @EventHandler
     public void onFakePlayerJoin(FakePlayerJoinEvent event) {
         Player plr = event.getFakePlayer().getEntityPlayer();
         plr.setViewDistance(0);
+        plugin.getLogger().warning("VIEW DISTANCE SET TO 0");
     }
 }
